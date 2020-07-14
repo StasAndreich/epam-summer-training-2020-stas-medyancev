e5ee4c1e6d7d21f33144b73e9a2e8eff89435e21
Стас Медянцев <smedyancev@gmail.com>
2020-07-14 23:32:14 +0300

    [task_2_1]: More of smart vector props added

--- MathTypes/Vector3.cs
+++ MathTypes/Vector3.cs
@@ -8,13 +8,51 @@ namespace MathTypes
     public class Vector3
     {
         /// <summary>
+        /// Shortcut property for creating Vector3(1, 1, 1).
+        /// </summary>
+        public static Vector3 One
+        {
+            get => new Vector3(1f, 1f, 1f);
+        }
+
+        /// <summary>
+        /// Shortcut property for creating Vector3(0, 0, 0).
+        /// </summary>
+        public static Vector3 Zero
+        {
+            get => new Vector3(0f, 0f, 0f);
+        }
+
+        /// <summary>
+        /// Shortcut property for creating Vector3(float.NegativeInfinity,
+        /// float.NegativeInfinity, float.NegativeInfinity).
+        /// </summary>
+        public static Vector3 NegativeInfinity
+        {
+            get => new Vector3(double.NegativeInfinity,
+                               double.NegativeInfinity,
+                               double.NegativeInfinity);
+        }
+
+        /// <summary>
+        /// Shortcut property for creating Vector3(float.PositiveInfinity,
+        /// float.PositiveInfinity, float.PositiveInfinity).
+        /// </summary>
+        public static Vector3 PositiveInfinity
+        {
+            get => new Vector3(double.PositiveInfinity,
+                               double.PositiveInfinity,
+                               double.PositiveInfinity);
+        }
+
+        /// <summary>
         /// Ctor that creates a new vector
         /// with (x, y, z) components.
         /// </summary>
         /// <param name="x">X-component.</param>
         /// <param name="y">Y-component.</param>
         /// <param name="z">Z-component.</param>
-        public Vector3(float x, float y, float z)
+        public Vector3(double x, double y, double z)
         {
             this.X = x;
             this.Y = y;
@@ -36,6 +74,21 @@ namespace MathTypes
         /// </summary>
         public double Z { get; protected set; }
 
+        /// <summary>
+        /// Returns the length of this vector (Read Only).
+        /// </summary>
+        public double Magnitude
+        {
+            get => (double)Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        /// <summary>
+        /// Returns squared length of this vector (Read Only).
+        /// </summary>
+        public double SqrMagnitude
+        {
+            get => (double)(X * X + Y * Y + Z * Z);
+        }
 
         /// <summary>
         /// Returns a formatted string for Vector3.
@@ -57,7 +110,9 @@ namespace MathTypes
             if (this.GetType() != obj.GetType()) return false;
 
             var vector3 = (Vector3)obj;
-            return this.X.Equals(vector3.X) && this.Y.Equals(vector3.Y) && this.Z.Equals(vector3.Z);
+            return this.X.Equals(vector3.X)
+                && this.Y.Equals(vector3.Y)
+                && this.Z.Equals(vector3.Z);
         }
 
         /// <summary>
