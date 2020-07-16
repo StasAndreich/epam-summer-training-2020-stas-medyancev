712bfdafe64c67ae55488c64cfee0d569a080c08
Стас Медянцев <smedyancev@gmail.com>
2020-07-16 14:39:31 +0300

    [task_2_2]: Basic structure of Monomial class added

---
+++ MathTypes/Monomial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MathTypes
+{
+    /// <summary>
+    /// Type that defines a monomial.
+    /// </summary>
+    public sealed class Monomial
+    {
+        /// <summary>
+        /// Ctor that creates a monomial using coef 
+        /// and degree values (by default: degree = 0).
+        /// </summary>
+        /// <param name="coef"></param>
+        /// <param name="degree"></param>
+        public Monomial(double coef, double degree = 0)
+        {
+            this.Coefficient = coef;
+            this.Degree = degree;
+        }
+
+        /// <summary>
+        /// Monomial coefficient value.
+        /// </summary>
+        public double Coefficient { get; set; }
+
+        /// <summary>
+        /// Monomial degree.
+        /// </summary>
+        public double Degree { get; set; }
+
+        /// <summary>
+        /// Inserts a parameter value in monomial.
+        /// </summary>
+        /// <param name="variableValue"></param>
+        /// <returns>Computed monomial result.</returns>
+        public double GetMonomialValue(double variableValue)
+        {
+            return Math.Pow(variableValue, Degree) * Coefficient;
+        }
+    }
+}
