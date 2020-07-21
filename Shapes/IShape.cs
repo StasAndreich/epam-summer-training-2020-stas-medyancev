0d33a98426e1e6faca7d72a6493c1fd6971a9e5b
Стас Медянцев <smedyancev@gmail.com>
2020-07-21 15:15:26 +0300

    [task03]: Shape lib created

---
+++ Shapes/IShape.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes
+{
+    /// <summary>
+    /// Contains definitions for a shape elements.
+    /// </summary>
+    interface IShape
+    {
+
+
+        string Name { get; }
+
+        double GetPerimeter();
+
+        double GetArea();
+    }
+}
