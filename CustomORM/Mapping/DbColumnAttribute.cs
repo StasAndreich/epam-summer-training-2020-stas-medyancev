a30b5b9639466e0323d53c85a51c836c400f9a2b
Стас Медянцев <smedyancev@gmail.com>
2020-08-28 17:48:20 +0300

    [task06]: DB Attributes added.

---
+++ CustomORM/Mapping/DbColumnAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CustomORM.Mapping
+{
+    /// <summary>
+    /// Associates a class with a column in a database table.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class DbColumnAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the CustomORM.Mapping.DbColumnAttribute class.
+        /// </summary>
+        public DbColumnAttribute() { }
+
+        /// <summary>
+        /// Gets or sets the name of a column.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets whether this class member represents a column 
+        /// that is part or all of the primary key of the table.
+        /// </summary>
+        /// <returns>Default = false.</returns>
+        public bool IsPrimaryKey { get; set; }
+        /// <summary>
+        /// Gets or sets whether a column can contain null values.
+        /// </summary>
+        /// <returns>Default = true.</returns>
+        public bool CanBeNull { get; set; } = true;
+    }
+}
---
+++ CustomORM/Mapping/DbTableAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomORM.Mapping
+{
+    /// <summary>
+    /// Designates a class as an entity class that is associated with a database table.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class DbTableAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the CustomORM.Mapping.DbTableAttribute class.
+        /// </summary>
+        public DbTableAttribute() { }
+
+        /// <summary>
+        /// Gets or sets the name of the table or view.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
