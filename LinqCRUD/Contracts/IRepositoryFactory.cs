04a71dcdb4857a147e8e2e4dcce5b2aab5faeb93
Стас Медянцев <smedyancev@gmail.com>
2020-09-20 21:47:04 +0300

    [task07]: Repository factory created.

---
+++ LinqCRUD/Contracts/IRepositoryFactory.cs
@@ -0,0 +1,44 @@
+﻿namespace LinqCRUD.Contracts
+{
+    /// <summary>
+    /// Defines all repositories for models.
+    /// </summary>
+    public interface IRepositoryFactory
+    {
+        /// <summary>
+        /// Creates Subject Repository.
+        /// </summary>
+        /// <returns></returns>
+        ISubjectRepository GetSubjectRepository();
+        /// <summary>
+        /// Creates Student Repository.
+        /// </summary>
+        /// <returns></returns>
+        IStudentRepository GetStudentRepository();
+        /// <summary>
+        /// Creates Group Repository.
+        /// </summary>
+        /// <returns></returns>
+        IGroupRepository GetGroupRepository();
+        /// <summary>
+        /// Creates Exam Repository.
+        /// </summary>
+        /// <returns></returns>
+        IExamRepository GetExamRepository();
+        /// <summary>
+        /// Creates Assessment Repository.
+        /// </summary>
+        /// <returns></returns>
+        IAssessmentRepository GetAssessmentRepository();
+        /// <summary>
+        /// Creates Assessment Repository.
+        /// </summary>
+        /// <returns></returns>
+        IExaminerRepository GetExaminerRepository();
+        /// <summary>
+        /// Creates Assessment Repository.
+        /// </summary>
+        /// <returns></returns>
+        ISpecialtyRepository GetSpecialtyRepository();
+    }
+}
---
+++ LinqCRUD/Repositories/RepositoryFactory.cs
@@ -0,0 +1,43 @@
+﻿using LinqCRUD.Contracts;
+using System;
+
+namespace LinqCRUD.Repositories
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        public IAssessmentRepository GetAssessmentRepository()
+        {
+            return new AssessmentRepository();
+        }
+
+        public IExaminerRepository GetExaminerRepository()
+        {
+            return new ExaminerRepository();
+        }
+
+        public IExamRepository GetExamRepository()
+        {
+            return new ExamRepository();
+        }
+
+        public IGroupRepository GetGroupRepository()
+        {
+            return new GroupRepository();
+        }
+
+        public ISpecialtyRepository GetSpecialtyRepository()
+        {
+            return new SpecialtyRepository();
+        }
+
+        public IStudentRepository GetStudentRepository()
+        {
+            return new StudentRepository();
+        }
+
+        public ISubjectRepository GetSubjectRepository()
+        {
+            return new SubjectRepository();
+        }
+    }
+}
